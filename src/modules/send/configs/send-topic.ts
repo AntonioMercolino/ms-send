9cecce14dd3ce71e65655e5e0ac108fc6e129be0
RebeccaRestaino <rebecca.restaino@protomgroup.com>
2024-08-02 10:21:13 +0200

    SendTopic

--- src/modules/send/configs/send-topic.ts
+++ src/modules/send/configs/send-topic.ts
@@ -1,5 +1,5 @@
 export enum SendTopic {
-   NOTIFICATION_SAVE_OR_UPDATE ="ms-geo.country.save-or-update",
-   NOTIFICATION_FIND =" ms-geo.country.find",
-   NOTIFICATION_DELETE = "ms-geo.country.delete",
+   NOTIFICATION_SAVE_OR_UPDATE ="ms-send.notification.save-or-update",
+   NOTIFICATION_FIND =" ms-send.notification.find",
+   NOTIFICATION_DELETE = "ms-send.notification.delete",
 }
